faf2e63d001f6f3facb9fbd9e65fef2a1b14b82d
Andres Otarola Alvarado <aotarola@groupon.com>
2015-11-08 22:02:39 -0800

    Added better compose button

--- app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activities/TimelineActivity.java
@@ -1,14 +1,10 @@
 package com.codepath.apps.mysimpletweets.activities;
 
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,17 +17,14 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
-
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.TwitterApplication;
 import com.codepath.apps.mysimpletweets.TwitterClient;
 import com.codepath.apps.mysimpletweets.adaptors.TweetsArrayAdaptor;
 import com.codepath.apps.mysimpletweets.interfaces.ComposeFragmentListener;
 import com.codepath.apps.mysimpletweets.listeners.CustomRecyclerViewListener;
-import com.codepath.apps.mysimpletweets.listeners.EndlessRecyclerOnScrollListener;
-import com.codepath.apps.mysimpletweets.listeners.HidingScrollListener;
 import com.codepath.apps.mysimpletweets.models.Tweet;
 import com.codepath.apps.mysimpletweets.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -41,15 +34,15 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class TimelineActivity extends AppCompatActivity implements ComposeFragmentListener {
 
     private TwitterClient client;
     private ArrayList<Tweet> tweets;
     private TweetsArrayAdaptor aTweets;
-    private RecyclerView lvTweets;
+    private RecyclerView rvTweets;
     private User currentUser;
+    private LinearLayout bottomLayout;
     Toolbar toolbar;
 
     private ImageButton ibCompose;
@@ -86,15 +79,16 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
         setContentView(R.layout.activity_timeline);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
+        bottomLayout = (LinearLayout) findViewById(R.id.llBottom);
         setSupportActionBar(toolbar);
         ibCompose = (ImageButton) findViewById(R.id.ibCompose);
-        lvTweets = (RecyclerView) findViewById(R.id.lvTweets);
+        rvTweets = (RecyclerView) findViewById(R.id.rvTweets);
         tweets = new ArrayList<>();
         aTweets = new TweetsArrayAdaptor(this, tweets);
-        lvTweets.setAdapter(aTweets);
+        rvTweets.setAdapter(aTweets);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        lvTweets.setLayoutManager(linearLayoutManager);
-        lvTweets.setOnScrollListener(new CustomRecyclerViewListener(linearLayoutManager) {
+        rvTweets.setLayoutManager(linearLayoutManager);
+        rvTweets.setOnScrollListener(new CustomRecyclerViewListener(linearLayoutManager) {
 
             @Override
             public void onLoadMore(int current_page) {
@@ -121,14 +115,14 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
     private void hideViews() {
         toolbar.animate().translationY(-toolbar.getHeight()).setInterpolator(new AccelerateInterpolator(2));
 
-        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) ibCompose.getLayoutParams();
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) bottomLayout.getLayoutParams();
         int fabBottomMargin = lp.bottomMargin;
-        ibCompose.animate().translationY(ibCompose.getHeight()+fabBottomMargin).setInterpolator(new AccelerateInterpolator(2)).start();
+        bottomLayout.animate().translationY(bottomLayout.getHeight()+fabBottomMargin).setInterpolator(new AccelerateInterpolator(2)).start();
     }
 
     private void showViews() {
         toolbar.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2));
-        ibCompose.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
+        bottomLayout.animate().translationY(0).setInterpolator(new DecelerateInterpolator(2)).start();
     }
 
     @Override
@@ -139,7 +133,7 @@ public class TimelineActivity extends AppCompatActivity implements ComposeFragme
         miGoTop.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-            lvTweets.smoothScrollToPosition(0);
+            rvTweets.smoothScrollToPosition(0);
             return true;
             }
         });
